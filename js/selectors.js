335184bf13bed622dd00fcb5b572e7c54ca6bc4d
Ryan Davey <ryancdavey@icloud.com>
2018-06-12 04:11:30 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,16 @@
 // declare your functions here...
+function paragraphSelector() {
+  $('p');
+}
+function lastImageSelector() {
+  $('div:last img:last');
+}
+function ninjaBabySelector() {
+  $('#baby-ninja');
+}
+function divSelector() {
+  $('div .pics');
+}
+function firstListItem() {
+  $('#pic-list ul:first-child');
+}
